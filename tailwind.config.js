90e2190270b598be28cb2f8653d1577a04634252
Kundan <kundanpatel2211@gmail.com>
2024-01-13 01:05:19 +0530

    Add All data

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        // Using modern `rgb`
+        primary: "rgb(var(--color-primary) / <alpha-value>)",
+        secondary: "rgb(var(--color-secondary) / <alpha-value>)",
+
+        // Using modern `hsl`
+        primary: "hsl(var(--color-primary) / <alpha-value>)",
+        secondary: "hsl(var(--color-secondary) / <alpha-value>)",
+
+        // Using legacy `rgba`
+        primary: "rgba(var(--color-primary), <alpha-value>)",
+        secondary: "rgba(var(--color-secondary), <alpha-value>)",
+      },
+    },
+  },
+  plugins: [],
+};
